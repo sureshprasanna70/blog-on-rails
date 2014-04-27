84e3871aff579f6dd2575aedb58e4b0023390855
sureshprasanna70 <sureshprasannaceg@gmail.com>
2014-04-27 23:46:23 +0530

    learning git flow

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fordecise::Application.config.secret_key_base = '7b8dab186ce759b2cd04baadea48e10455860b1862f472855cefaa74c9b765091dd20013a73305b51fe72d178276cba502b26b35ed6cfc81b3ec61cc53ff3358'
