32ce9d097fb016bd5856b220c3465370343b17c0
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-09-20 00:17:48 +0530

    Landing page

---
+++ app/views/home/index1.html.rb
@@ -0,0 +1,75 @@
+
+<div class="container" id="home">
+<div class="col-md-3">
+  <nav class="nav navbar-default sidebar">
+    <ul class="nav navbar-nav">
+      <li><a href=""><i class="fa fa-share"></i> Share</a></li>
+      <li><a href=""><i class="fa fa-thumbs-o-up"></i> Like</a></li>
+    </ul>
+  </nav>
+</div>
+<div class="col-md-9">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="/assets/c.jpg" height="250px" width="200px" alt="First slide">
+        <div class="container">
+          <div class="carousel-caption">
+            <p>Added desc</p>
+            <!--p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p-->
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="/assets/geek.jpg" height="250px" width="200px" alt="Second slide">
+        <div class="container">
+          <div class="carousel-caption">
+            <p>Added desc</p>
+            
+            <!--p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p-->
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="/assets/crypt.jpg" height="250px" width="200px" alt="Third slide">
+        <div class="container">
+          <div class="carousel-caption">
+            <p>Added desc</p>
+            <!--p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p-->
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div><!-- /.carousel -->
+  <p>Welcome to the official site of CEG-SPARTANZ.sdhfkjsdhfkdsjhfksdhfkjsdhfkjs<br>dhfkjdshfkdjshfkdj<br>sfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfh<br>kdjsfhksdhfkdjshfksdjhfkjsdfhfhk djsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfk djsh fksdjhfkjsdfhfhkdjsfhkdjsf<br>
+    kdjsfhkdjsfhkdjsfhksdhfkdjshfksdjh fkjsdfhfhkdj sfhkdjsfhkdjsfhkdjsfhkd<br>jsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhbr>kdjsfhkdjsf hkdjsfhkdjsfh<br>
+    ksdhfk djshfks dj hfkj sdfhfh kdjsf hkdjsfhk djsfhkdjsfh kdjsfhksdhfkdjshfksdjhfkjsdfhfhk djsfhkdjsfhkdjs <br>
+    fhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjs<br>
+    fhksdh<br>fk djshfksd jhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjs<br>fhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfh<br>kdjsfhksdhfkdjshfksdjhfkjsdf<br>
+    hfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjs<br>
+    fhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhk<br>
+    djsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjh<br>
+    fkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfk<br>
+    jsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfh</p>
+  
+  <p>Welcome to the official site of CEG-SPARTANZ.sdhfkjsdhfkdsjhfksdhfkjsdhfkjs<br>dhfkjdshfkdjshfkdj<br>sfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfh<br>kdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsf<br>
+    kdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkd<br>jsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhbr>kdjsfhkdjsfhkdjsfhkdjsfh<br>
+    ksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjs <br>
+    fhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjs<br>
+    fhksdh<br>fkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjs<br>fhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfh<br>kdjsfhksdhfkdjshfksdjhfkjsdf<br>
+    hfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjs<br>
+    fhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhk<br>
+    djsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjh<br>
+    fkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfk<br>
+    jsdfhfhkdjsfhkdjsfhkdjsfhkdjsfhkdjsfhksdhfkdjshfksdjhfkjsdfh</p>
+</div>
+
+
+</div>
